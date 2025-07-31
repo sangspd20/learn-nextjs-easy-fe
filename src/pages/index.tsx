484ccc38e67ab945fbcbf59ec28170c420f0a719
F88\sangnt3 <sangnt3@f88.vn>
2025-07-31 16:32:13 +0700

    4.20_seo_for_home_page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Seo } from '@/components/common'
 import { FeatureWorks, HeroSection, RecentPosts } from '@/components/home'
 import { MainLayout } from '@/components/layout'
 import { Box } from '@mui/material'
@@ -5,6 +6,17 @@ import { Box } from '@mui/material'
 export default function Home() {
   return (
     <Box>
+      <Seo
+        data={{
+          title: 'NextJS Tutorials | Easy Frontend',
+          description:
+            'Step by step tutorials to build a full CRUD website using NextJS for beginners',
+          url: 'https://learn-nextjs-fawn.vercel.app/',
+          thumbnailUrl:
+            'https://cdn.getshifter.co/caa65008efb706a8bfc6f7e4045d6a018420c3df/uploads/2020/11/nextjs.png',
+        }}
+      />
+
       <HeroSection />
       <RecentPosts />
       <FeatureWorks />
