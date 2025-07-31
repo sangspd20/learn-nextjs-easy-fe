ccd7c86654b6d9b092e58faf036cfdb3f413eb8f
F88\sangnt3 <sangnt3@f88.vn>
2025-07-31 16:26:45 +0700

    4.19_footer

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,10 +1,33 @@
 import { Box } from '@mui/material'
 import * as React from 'react'
+import { Facebook, Instagram, LinkedIn, Twitter } from '@mui/icons-material'
+import { Icon, Stack, Typography } from '@mui/material'
 
 export function Footer() {
+  const socialLinks = [
+    { icon: Facebook, url: 'https://google.com' },
+    { icon: Instagram, url: 'https://google.com' },
+    { icon: Twitter, url: 'https://google.com' },
+    { icon: LinkedIn, url: 'https://google.com' },
+  ]
   return (
     <Box component="footer" py={2} textAlign="center">
-      Footer
+      <Stack direction="row" justifyContent="center">
+        {socialLinks.map((item, idx) => (
+          <Box
+            key={idx}
+            component="a"
+            p={2}
+            href={item.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon component={item.icon} sx={{ fontSize: 48 }} />
+          </Box>
+        ))}
+      </Stack>
+
+      <Typography>Copyright ©{new Date().getFullYear()} All rights reserved</Typography>
     </Box>
   )
 }
