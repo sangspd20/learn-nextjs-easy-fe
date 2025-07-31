9de2a5e628d62f12cce133b37bd85376ce382a28
F88\sangnt3 <sangnt3@f88.vn>
2025-07-31 14:45:24 +0700

    4.14_post_card

--- src/components/home/post-card.tsx
+++ src/components/home/post-card.tsx
@@ -1,18 +1,28 @@
-import { Card, CardContent, Typography } from '@mui/material'
+import { Card, CardContent, Typography, Divider } from '@mui/material'
 import * as React from 'react'
+import { Post } from '@/models/index'
+import { format } from 'date-fns'
+export interface PostCardProps {
+  post: Post
+}
 
-export interface PostCardProps {}
-
-export function PostCard(props: PostCardProps) {
+export function PostCard({ post }: PostCardProps) {
+  if (!post) return null
   return (
     <Card>
       <CardContent>
-        <Typography>Title</Typography>
-        <Typography>Title</Typography>
-        <Typography>Title</Typography>
-        <Typography>Title</Typography>
-        <Typography>Title</Typography>
-        <Typography>Title</Typography>
+        <Typography variant="h5" fontWeight="bold">
+          {post.title}
+        </Typography>
+
+        <Typography variant="body1" my={2} sx={{ display: 'flex' }}>
+          {format(Number(post.publishedDate), 'dd MMM yyyy')}
+
+          <Divider orientation="vertical" sx={{ mx: 2 }} flexItem />
+
+          {post.tagList.join(', ')}
+        </Typography>
+        <Typography variant="body2">{post.description}</Typography>
       </CardContent>
     </Card>
   )
--- src/components/home/recent-posts.tsx
+++ src/components/home/recent-posts.tsx
@@ -2,8 +2,27 @@ import { Box, Container, Link as MuiLink, Stack, Typography } from '@mui/materia
 import NextLink from 'next/link'
 import * as React from 'react'
 import { PostCard } from './post-card'
+import { Post } from '@/models'
 
 export function RecentPosts() {
+  const postList: Post[] = [
+    {
+      id: '1',
+      title: 'Making a design system from scratch',
+      publishedDate: '1648363391671',
+      tagList: ['Design', 'Pattern'],
+      description:
+        'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+    },
+    {
+      id: '2',
+      title: 'Creating pixel perfect icons in Figma',
+      publishedDate: '1648363391671',
+      tagList: ['Figma', 'Icon Design'],
+      description:
+        'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+    },
+  ]
   return (
     <Box component="section" bgcolor="secondary.light" pt={2} pb={4}>
       <Container>
@@ -41,13 +60,11 @@ export function RecentPosts() {
             },
           }}
         >
-          <Box>
-            <PostCard />
-          </Box>
-
-          <Box>
-            <PostCard />
-          </Box>
+          {postList.map((post) => (
+            <Box key={post.id}>
+              <PostCard post={post} />
+            </Box>
+          ))}
         </Stack>
       </Container>
     </Box>
