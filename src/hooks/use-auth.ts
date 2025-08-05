7d50fafbd2e9d8491aed045494e4c73ac8489194
F88\sangnt3 <sangnt3@f88.vn>
2025-08-05 10:49:33 +0700

    6.10_add_type_for_use_auth

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -1,5 +1,5 @@
 import { authApi } from '@/api-clients/auth-api'
-import { LoginPayload } from '@/models'
+import { LoginPayload, UserProfile } from '@/models'
 import useSWR from 'swr'
 import { PublicConfiguration } from 'swr/_internal'
 
@@ -8,8 +8,8 @@ export function useAuth(options?: Partial<PublicConfiguration>) {
     data: profile,
     error,
     mutate,
-  } = useSWR('/profile', {
-    dedupingInterval: 60000,
+  } = useSWR<UserProfile | null>('/profile', {
+    dedupingInterval: 60 * 60 * 1000, // 1hr
     revalidateOnFocus: false,
     ...options,
   })
