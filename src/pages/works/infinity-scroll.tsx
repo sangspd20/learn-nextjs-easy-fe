c93ade145d992ea18492b8c54e8283d69e2dbf16
sang nguyen <sangspd20@gmail.com>
2025-08-08 15:33:22 +0700

    6.29_useWorkListInfInity

---
+++ src/pages/works/infinity-scroll.tsx
@@ -0,0 +1,82 @@
+import { MainLayout } from '@/components/layout'
+import { WorkFilters, WorkList } from '@/components/work'
+import { useWorkListInfinity } from '@/hooks'
+import { ListParams, WorkFiltersPayload } from '@/models'
+import { Box, Container, Skeleton, Typography } from '@mui/material'
+import { useRouter } from 'next/router'
+
+export default function WorksPage() {
+  const router = useRouter()
+  const filters: Partial<ListParams> = {
+    ...router.query,
+  }
+  const initFiltersPayload: WorkFiltersPayload = {
+    search: filters.title_like || '',
+    selectedTagList: filters.tagList_like?.split('|') || [],
+  }
+
+  const { data, isLoading, isValidating, size, setSize } = useWorkListInfinity({
+    params: filters,
+    enabled: router.isReady,
+  })
+  console.log({ data, isLoading, isValidating, size })
+  // const { _limit, _totalRows, _page } = data?.pagination || {}
+  // const totalPages = Boolean(_totalRows) ? Math.ceil(_totalRows / _limit) : 0
+
+  function handleFiltersChange(newFilters: WorkFiltersPayload) {
+    router.push(
+      {
+        pathname: router.pathname,
+        query: {
+          ...filters,
+          _page: 1,
+          title_like: newFilters.search,
+          tagList_like: newFilters.tagList_like,
+        },
+      },
+      undefined,
+      { shallow: true },
+    )
+  }
+
+  return (
+    <Box>
+      <Container>
+        <Box mb={4} mt={8}>
+          <Typography component="h1" variant="h3" fontWeight="bold">
+            Work
+          </Typography>
+        </Box>
+
+        {router.isReady ? (
+          <WorkFilters initialValues={initFiltersPayload} onSubmit={handleFiltersChange} />
+        ) : (
+          <Skeleton
+            variant="rectangular"
+            height={40}
+            sx={{
+              display: 'inline-block',
+              width: '100%',
+              mt: 2,
+              mb: 1,
+              verticalAlign: 'middle',
+            }}
+          />
+        )}
+
+        <WorkList workList={[]} loading={!router.isReady || isLoading} />
+      </Container>
+    </Box>
+  )
+}
+
+WorksPage.Layout = MainLayout
+
+export async function getStaticProps() {
+  console.log('get static props')
+  // const workList = await workA
+
+  return {
+    props: {},
+  }
+}
